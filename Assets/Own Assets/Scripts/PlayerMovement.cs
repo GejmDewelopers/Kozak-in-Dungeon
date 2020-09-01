4c2860be09e44ce49100a43e3d93c8c17c45f5a8
Rafikolcz <rafikolcz@gmail.com>
2020-09-02 00:06:17 +0200

    Fucked up your tilemap, added some from brackeys. added shooting

--- Assets/Own Assets/Scripts/PlayerMovement.cs
+++ Assets/Own Assets/Scripts/PlayerMovement.cs
@@ -5,44 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Camera cam;
     Animator animator;
-    SpriteRenderer spriteRenderer;
+
+    Vector2 movement;
+    Vector2 mousePos;
+    SpriteRenderer spr; //TODO: Remove everything about it and replace with animations
     void Start()
     {
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        if (movement.x < 0) spr.flipX = true;
+        if(movement.x > 0) spr.flipX = false;
     }
 
     private void FixedUpdate()
     {
-        Vector2 position = transform.position;
-        if (Input.GetAxisRaw("Horizontal") > 0.1f)
-        {
-            position.x += speed * Time.fixedDeltaTime;
-            transform.position = position;
-            spriteRenderer.flipX = false;
-        }
-        if (Input.GetAxisRaw("Horizontal") < -0.1f)
-        {
-            position.x -= speed * Time.fixedDeltaTime;
-            transform.position = position;
-            spriteRenderer.flipX = true;
-        }
-        if (Input.GetAxisRaw("Vertical") > 0.1f)
-        {
-            position.y += speed * Time.fixedDeltaTime;
-            transform.position = position;
-        }
-        if (Input.GetAxisRaw("Vertical") < -0.1f)
-        {
-            position.y -= speed * Time.fixedDeltaTime;
-            transform.position = position;
-        }
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
     }
 }
---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse); // TODO: maybe firepoint.up later
+        Destroy(bullet.gameObject, 5f);
+    }
+}
