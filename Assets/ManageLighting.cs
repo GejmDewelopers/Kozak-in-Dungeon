214cff6f37fee1b78507f401ca9a79c33e01bd97
Rafikolcz <rafikolcz@gmail.com>
2020-09-12 22:14:17 +0200

    Added FULLY FUNCTIONAL UI

---
+++ Assets/ManageLighting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageLighting : MonoBehaviour
+{
+    [SerializeField] GameObject light2d;
+    void TurnLightOn()
+    {
+        light2d.SetActive(true);
+    }
+    void TurnLightOff()
+    {
+        light2d.SetActive(false);
+    }
+}
---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public AudioMixer audioMixer;
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        print(qualityIndex);
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainMenu.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
